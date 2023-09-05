b394040f552af14453b11b80466a9dc744f00ee5
Jackson <jacksonwhiting00@gmail.com>
2023-09-05 10:54:18 -0400

    revised code to fix the firefox issue.  Used the API URL that includes an ID so we don't get the 2 second cache which caused problems for firefox

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+const generateRandomNum = () => {
+	let min = 0
+	let max = 200
+	let randomNum = Math.floor(Math.random() * (max - min)) + min
+	return randomNum
+}
+
 const fetchAdvice = async () => {
-	const response = await fetch("https://api.adviceslip.com/advice")
+	// Generates a random number between 1 & 200.  The API doesn't allow IDs much over 200 so I chose 200
+	const idNum = generateRandomNum()
+
+	const response = await fetch(`https://api.adviceslip.com/advice/${idNum}`)
 	if (response.ok) {
 		const adviceObject = await response.json()
-		return adviceObject
+		const { id, advice } = adviceObject.slip
+		return { id, advice }
 	} else {
 		throw new Error("An error occured while fetching data")
 	}
@@ -14,8 +25,8 @@ let renderAdvice = () => {
 			const adviceTitle = document.querySelector("#advice-title")
 			const adviceStatement = document.querySelector("#advice-statement")
 
-			adviceTitle.textContent = `Advice #${data.slip.id}`
-			adviceStatement.textContent = `"${data.slip.advice}"`
+			adviceTitle.textContent = `Advice #${data.id}`
+			adviceStatement.textContent = `"${data.advice}"`
 		})
 		.catch((err) => {
 			console.log(err)
@@ -30,16 +41,6 @@ renderAdvice()
 	generateIcon.addEventListener("click", (e) => {
 		e.preventDefault()
 
-		fetchAdvice()
-			.then((data) => {
-				const adviceTitle = document.querySelector("#advice-title")
-				const adviceStatement = document.querySelector("#advice-statement")
-
-				adviceTitle.textContent = `Advice #${data.slip.id}`
-				adviceStatement.textContent = `"${data.slip.advice}"`
-			})
-			.catch((err) => {
-				console.log(err)
-			})
+		renderAdvice()
 	})
 })()
